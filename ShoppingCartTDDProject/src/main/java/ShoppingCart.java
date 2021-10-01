155f07268e121ce8d1d5b52dcd5d8f42d37bd970
Jack Old <jack.old@outlook.com>
2021-10-01 16:21:47 -0400

    Added Shopping Cart functionality and test cases

--- ShoppingCartTDDProject/src/main/java/ShoppingCart.java
+++ ShoppingCartTDDProject/src/main/java/ShoppingCart.java
@@ -16,17 +16,43 @@ public class ShoppingCart {
         return cart.size();
     }
 
+    public Item getItemFromCart(String key){
+        return cart.get(key);
+    }
+
     public void addItemToCart(Item item){
-        cart.put(item.getItemName(), item);
+
+        if(cart.containsKey(item.getItemName())){
+            Item cartItem = cart.get(item.getItemName());
+            int newQuantity = cartItem.getQuantity() + item.getQuantity();
+            item.setQuantity(newQuantity);
+            cart.replace(item.getItemName(), cartItem, item);
+
+        }
+        else{
+            cart.put(item.getItemName(), item);
+        }
     }
 
     public double getSubtotal(){
         double subtotal = 0;
         for(Item item : cart.values()){
-            subtotal += item.getItemPrice();
+            double value = item.getItemPrice() * item.getQuantity();
+            subtotal += value;
         }
 
         return subtotal;
+    }
+
+    public String viewCart(){
+        String result = "";
+        for(Item item: cart.values()){
+            result += item.toString();
+        }
+
+
+
+        return result;
     }
 
 
--- ShoppingCartTDDProject/src/test/java/ShoppingCartTest.java
+++ ShoppingCartTDDProject/src/test/java/ShoppingCartTest.java
@@ -28,6 +28,40 @@ public class ShoppingCartTest {
         assertEquals(2.50, sc.getSubtotal() );
     }
 
+    @Test
+    public void checkPriceSubTotalMultiItem(){
+        //sc.getCartSize();
+        sc.addItemToCart(new Item("Bread", 2.50, 1));
+        sc.addItemToCart(new Item("Chicken", 1.00, 2));
+        assertEquals(4.50, sc.getSubtotal());
+    }
+
+    @Test
+    public void checkItemQuantityMultiEntries(){
+        sc.addItemToCart(new Item("Bread", 2.50, 1));
+        sc.addItemToCart(new Item("Bread", 2.50, 2));
+        Item item = sc.getItemFromCart("Bread");
+        assertEquals(3, item.getQuantity());
+    }
+
+    @Test
+    public void listItemsInCart(){
+        sc.addItemToCart(new Item("Bread", 2.50, 1));
+        sc.addItemToCart(new Item("Chicken", 1.00, 2));
+        sc.addItemToCart(new Item("Bread", 2.50, 2));
+        //System.out.println(sc.viewCart());
+        assertEquals("Item{itemName='Chicken', itemPrice=1.0, quantity=2}Item{itemName='Bread', itemPrice=2.5, quantity=3}", sc.viewCart());
+    }
+
+    @Test
+    public void getSubTotalOfItemsInCart(){
+        sc.addItemToCart(new Item("Bread", 2.50, 1));
+        sc.addItemToCart(new Item("Chicken", 1.00, 2));
+        sc.addItemToCart(new Item("Bread", 2.50, 2));
+
+        assertEquals(9.50, sc.getSubtotal());
+    }
+
 
 
 }
